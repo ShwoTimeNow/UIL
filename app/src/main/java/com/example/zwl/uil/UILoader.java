24f5e9278f17a80d834d96c1ec8a8d0c930e6815
ZWL <weilongzhang16@gmail.com>
2015-01-05 18:48:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zwl/uil/UILoader.java
@@ -0,0 +1,18 @@
+package com.example.zwl.uil;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ZWL on 2014/12/29.
+ */
+public class UILoader extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+}
